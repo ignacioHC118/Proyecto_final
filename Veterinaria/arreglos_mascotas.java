cb8c230afdcd7f2884816b4ef4637e60156a0985
ignacioHC118 <N00372292@upn.pe>
2025-05-22 15:37:27 -0500

    Update arreglos_mascotas.java

--- Veterinaria/arreglos_mascotas.java
+++ Veterinaria/arreglos_mascotas.java
@@ -13,23 +13,5 @@ public class Arreglo_mascotas {
 		
 	}
 	
-	public void Adicionar(Mascota x) {
-		masco.add(x);
-	}
-	public int Tamaño() {
-		return masco.size();
-	}
-	public Mascota Obtener(int x) {
-		return masco.get(x);
-	}
 	
-	public Mascota Buscar(String nombre) {
-		for (int i = 0; i < Tamaño(); i++) {
-			if(Obtener(i).getNom_pac()==nombre)return Obtener(i);
-		}
-		return null;
-	}
-	public void Eliminar(Mascota x) {
-		masco.remove(x);
-	}
 }
